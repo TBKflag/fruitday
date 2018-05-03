ccb344d39aeb00e22eefb794447cc37ede67ef5e
travis <123369291@qq.com>
2018-05-03 21:11:20 +0800

    fruitday的后台管理

--- index/admin.py
+++ index/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+class GoodAdmin(admin.ModelAdmin):
+    list_display = ['id','title', 'price', 'spec', 'picture', 'isActive']
+    list_editable=['title','price','spec','picture','isActive']
+    search_fields=['title','spec']
+    list_filter=['price']
+
+class userAdmin(admin.ModelAdmin):
+    list_display = ['id','uname', 'uphone', 'uemail', 'isActive']
+    list_editable = ['uname','uphone', 'uemail', 'isActive']
+    search_fields=['uname','uphone','uemail']
+
+class goodtypeAdmin(admin.ModelAdmin):
+    list_display = ['id','title', 'desc', 'picture']
+    list_editable = ['title','desc', 'picture']
+
+
+admin.site.register(Users, userAdmin)
+admin.site.register(GoodsType, goodtypeAdmin)
+admin.site.register(Goods, GoodAdmin)
--- index/models.py
+++ index/models.py
@@ -4,22 +4,41 @@ from django.db import models
 
 
 class Users(models.Model):
-    uphone = models.CharField(max_length=20)
-    upwd = models.CharField(max_length=30)
-    uemail = models.EmailField()
-    uname = models.CharField(max_length=30)
-    isActive = models.BooleanField(default=True)
-
+    uphone = models.CharField(max_length=20,verbose_name='电话号码')
+    upwd = models.CharField(max_length=30,verbose_name='密码')
+    uemail = models.EmailField(verbose_name='邮箱')
+    uname = models.CharField(max_length=30,verbose_name='用户名')
+    isActive = models.BooleanField(default=True, verbose_name='启用')
+    def __str__(self):
+        return self.uname
+    class Meta:
+        db_table = 'users'
+        verbose_name='用户'
+        verbose_name_plural=verbose_name
 
 class GoodsType(models.Model):
-    title = models.CharField(max_length=30)
-    desc = models.TextField()
-    picture = models.ImageField(upload_to='static/upload/goodstype')
+    title = models.CharField(max_length=30, verbose_name='类别')
+    desc = models.TextField(verbose_name='描述')
+    picture = models.ImageField(
+        upload_to='static/upload/goodstype', verbose_name='图片')
+    def __str__(self):
+        return self.title
+    class Meta:
+        db_table = 'goodsytpe'
+        verbose_name='商品类别'
+        verbose_name_plural=verbose_name
 
 
 class Goods(models.Model):
-    title = models.CharField(max_length=30)
-    price = models.DecimalField(max_digits=7,decimal_places=2)
-    spec = models.CharField(max_length=30)
-    picture=models.ImageField(upload_to='static/upload/goods')
-    isActive=models.BooleanField(default=True)
+    title = models.CharField(max_length=30,verbose_name='商品类别')
+    price = models.DecimalField(max_digits=7,decimal_places=2,verbose_name='价格')
+    spec = models.CharField(max_length=30,verbose_name='介绍')
+    picture=models.ImageField(upload_to='static/upload/goods',verbose_name='图片')
+    isActive = models.BooleanField(default=True, verbose_name='启用')
+    def __str__(self):
+        return self.title
+    class Meta:
+        db_table = 'goods'
+        verbose_name='商品'
+        verbose_name_plural=verbose_name
+
